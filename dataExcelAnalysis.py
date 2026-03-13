1fecb1bb6d587fade9b2517869b2a614b09221ee
subhayand10 <subhayan.sd10@gmail.com>
2026-03-13 11:22:03 +0530

    changes

--- dataExcelAnalysis.py
+++ dataExcelAnalysis.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import os
 
-df = pd.read_excel("dummy_patch_servers.xlsx")
+directory = 'C:\\Users\\SUBHAYAN\\Downloads\\Sample AI agent\\Excels' 
+filename = 'my_data.xlsx'
+full_path = os.path.join(directory, filename)
+
+df = pd.read_excel(full_path)
 filtered_df = df[df["Application Name"].str.contains("lyric", case=False, na=False)]
 server_list = filtered_df.to_dict(orient="records")
 print(server_list)
